0ea7eacbe95c033f95621c93f0c22f9d49ca8e74
Balaji S <sbalaji272006@gmail.com>
2025-06-18 20:05:29 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,15 +5,7 @@ import { getFirestore } from 'firebase/firestore'; // For Firestore database ser
 
 // Your Firebase configuration object from the Firebase Console
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAPQyjI1Wqe2Kb5FviRNPJiVTlZXoRHnCE",
-  authDomain: "expensetrackerapp-32672.firebaseapp.com",
-  projectId: "expensetrackerapp-32672",
-  storageBucket: "expensetrackerapp-32672.firebasestorage.app",
-  messagingSenderId: "66955718426",
-  appId: "1:66955718426:web:52317fd4ad782295bf46a3",
-  measurementId: "G-VDZTVGPPPS"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
